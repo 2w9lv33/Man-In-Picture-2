a65cfff21f9e5ca7a557c9fc6a89cab5d52a8869
2w9lv33 <823271351@qq.com>
2019-01-23 12:28:08 +0800

    s

--- Man-In-Picture-Test/Assets/Scripts/ItemScripts/Picture.cs
+++ Man-In-Picture-Test/Assets/Scripts/ItemScripts/Picture.cs
@@ -18,10 +18,28 @@ public class Picture : MonoBehaviour
     void Update()
     {
         myColor = Color.myColor;
+        ChangeColor();
+    }
+    public void ChangeColor()
+    {
+        if(transform.tag == "Enemy")
+        {
+            if(myColor == Game.Color.MyColor.NOCOLOR)
+            {
+                transform.parent.transform.Find("after").gameObject.SetActive(true);
+                transform.gameObject.SetActive(false);
+            }
+        }
         switch (myColor)
         {
             case Game.Color.MyColor.RED:
                 transform.GetComponent<Image>().color = UnityEngine.Color.red;
+                break;
+            case Game.Color.MyColor.ORANGE:
+                transform.GetComponent<Image>().color = Game.Color.Orange;
+                break;
+            case Game.Color.MyColor.PURPLE:
+                transform.GetComponent<Image>().color = Game.Color.Purple;
                 break;
             case Game.Color.MyColor.CYAN:
                 transform.GetComponent<Image>().color = UnityEngine.Color.cyan;
@@ -31,6 +49,9 @@ public class Picture : MonoBehaviour
                 break;
             case Game.Color.MyColor.YELLOW:
                 transform.GetComponent<Image>().color = UnityEngine.Color.yellow;
+                break;
+            case Game.Color.MyColor.GREEN:
+                transform.GetComponent<Image>().color = UnityEngine.Color.green;
                 break;
             case Game.Color.MyColor.BLACK:
                 transform.GetComponent<Image>().color = UnityEngine.Color.gray;
